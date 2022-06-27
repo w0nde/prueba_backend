d04ed311a797bcba8e4b3acd2255a736c04cf008
w0nder <campos.herrera.oscar@gmail.com>
2022-06-27 10:59:29 -0500

    ejercicio02

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import requests
+import gzip
 
-def get_value(name, status):
+
+def get_value(name, status, download):
     url = f'https://rickandmortyapi.com/api/character/?name={name}&status={status}'
     response = requests.get(url)
-    print(response.json())
+    data = response.json()
+    if download:
+        download_file(str(data))
+    print()
 
 
 def filter_character():
     name = str(input('enter name: '))
     status = str(input('enter status: '))
-    get_value(name, status)
+    download = bool(input('download ? : '))
+    get_value(name, status, download)
+
+
+def download_file(data):
+    encoded = data.encode('utf-8')
+    compressed = gzip.compress(encoded)
+    print(compressed)
+    f = open("response.zip", "wb")
+    f.write(compressed)
+    f.close()
 
 
 if __name__ == '__main__':
